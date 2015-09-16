e9372f11244fd907e43819a9872429659203e528
htmelvis <ed@htmelvis.com>
2015-09-15 23:19:49 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+const ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  context: ROOT_PATH,
+  entry:  [
+    'webpack-dev-server/client?http://127.0.0.1:8080',
+    'webpack/hot/only-dev-server',
+    ROOT_PATH + '/index.js'],
+  output: {
+    path: ROOT_PATH + '/public',
+    filename: 'bundle.js',
+    publicPath: 'http://127.0.0.1:8080/'
+  },
+  module: {
+    loaders: [
+      //think of babel vs jsx-loader
+      {test: /\.js?$/, exclude:/node_modules/,loaders: ['react-hot','babel-loader'], include: path.join(__dirname, 'public')},
+      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot','babel']},
+      {test: /\.(sass|scss)?$/, loaders: ['style','css','sass']}
+    ]
+  },
+  plugins: [
+    //extract inline css into a main style.css fil
+    //new ExtractTextPlugin('public/styles.css', {
+    //  allChunks: true
+    //}),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './public',
+    hot: true,
+    inline: true
+  }
+};
